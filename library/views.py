20eda18927039cfb0b0861a0e5ed0d6336a0ff00
Islam123-ux <maga.siyai777@gmail.com>
2025-04-26 09:21:54 +0300

    2

--- library/views.py
+++ library/views.py
@@ -8,7 +8,13 @@ def http_response(request):
 def render_response(request):
     return render(request, 'index.html')   
 
-def post_list_view  (request):
+def post_list_view(request):
     if request.method == "GET":
         posts = Post.objects.all()
         return render(request, 'posts/post_list.html', {'posts': posts})
+    
+
+def post_detail_view(request, id):
+    if request.method == "GET":
+       post = Post.objects.get(id=id)
+       return render(request, 'posts/post_detail.html', {'post': post})

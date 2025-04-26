6191d7480922d32d2f274494ac2d81d3ca981282
Islam123-ux <maga.siyai777@gmail.com>
2025-04-26 08:35:54 +0300

    удалил лайбрари

--- library/views.py
+++ library/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post 
 
 def http_response(request):
     return HttpResponse("Hello, world by Http!")
 
 def render_response(request):
-    return render(request, 'library/index.html')    
+    return render(request, 'index.html')   
+
+def post_list_view  (request):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        return render(request, 'posts/post_list.html', {'posts': posts})
